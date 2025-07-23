8c5d8f56a11a7981616c08fbfe830c565a69a2cd
Daniel <danielpontep@hotmail.com>
2025-07-23 17:15:01 -0300

    commit2307251714

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ PORT = 3080;
 
 //'development'
 
-if(process.env.NODE_ENV==='production'){
+if(process.env.NODE_ENV==='development'){
 
    app.use(express.static('frontend/build'));
    const path = require('path');
